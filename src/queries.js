ca973b4a9c96cabad31a753a561d992c508d7139
sarori <salo3678@gmail.com>
2020-03-19 23:22:04 +0100

    .

--- src/queries.js
+++ src/queries.js
@@ -1,21 +1,33 @@
 import gql from "graphql-tag";
 
 export const HOME_PAGE = gql`
-	query {
+	{
 		movies(limit: 50, rating: 7) {
 			id
 			title
 			rating
+			genres
+			medium_cover_image
 		}
 	}
 `;
 
-// export const HOME_PAGE = gql`
-// 	query movies($limit: number, $rating: number) {
-// 		movies(limit: $limit, rating: $rating) {
-// 			id
-// 			title
-// 			rating
-// 		}
-// 	}
-// `;
+export const MOVIE_DETAILS = gql`
+	query getMovieDetails($movieId: String!) {
+		movie(id: $movieId) {
+			medium_cover_image
+			title
+			rating
+			summary
+			language
+			genres
+		}
+		suggestions(id: $movieId) {
+			id
+			medium_cover_image
+			title
+			rating
+			genres
+		}
+	}
+`;
